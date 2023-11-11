6696aead11ec62acd5fbf7c9151b74e69ee5180a
Emil Mustafin <emill.mustafin@yandex.ru>
2023-11-12 02:29:04 +0300

    chore: update .eslintrc and .prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,58 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:perfectionist/recommended-natural',
+        'plugin:import/recommended',
+        'plugin:prettier/recommended',
     ],
-  },
-}
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react-refresh',
+        'react',
+        'react-hooks',
+        'import',
+        'perfectionist',
+        'prettier',
+    ],
+    rules: {
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-console': 'error',
+        'no-unused-expressions': 'warn',
+        'no-unused-vars': 'warn',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'react/jsx-no-useless-fragment': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+};
