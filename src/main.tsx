27ca544c8b1764cc3e500609bb024fa07ba02022
Emil Mustafin <emill.mustafin@yandex.ru>
2023-11-13 03:33:47 +0300

    force:fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Aston-fakeShop">
       <Provider store={store}>
         <App />
       </Provider>
